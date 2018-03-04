7934623abcae392a8f074694be62e01bdad717e1
tsiper <tsiper@gmail.com>
2018-03-04 16:36:11 +0200

    fixed some zshrc trouble

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,6 +1,9 @@
 # Installing zsh
 sudo apt install -y zsh
+
+# Changing default shell to zsh for EC2 computers as well
 chsh -s $(which zsh)
+sudo chsh ubuntu -s $(which zsh)
 
 # Installing antigen
 curl -L git.io/antigen > antigen.zsh
